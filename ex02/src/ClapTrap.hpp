14a0dea308d60896a9bfcdcb2d2c93117ebc8d83
Shun Usami <usatie@gmail.com>
2023-03-28 15:26:03 +0900

    :sparkles: Init ex02

---
+++ ex02/src/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <string>
+
+class ClapTrap {
+protected:
+	ClapTrap( std::string n, int h, int e, int a ) ;
+	std::string _name ;
+	int	_hitPoints ;
+	int	_energyPoints ;
+	int	_attackDamage ;
+public:
+	ClapTrap() ;
+	ClapTrap( const std::string & name ) ;
+	ClapTrap(const ClapTrap& x) ;
+	ClapTrap& operator =(const ClapTrap& x) ;
+	virtual ~ClapTrap() ;
+	// member functions
+	virtual void	attack(const std::string& target) ;
+	virtual void	takeDamage(unsigned int amount) ;
+	virtual void	beRepaired(unsigned int amount) ;
+} ;
+
+#endif
---
+++ ex02/src/ScavTrap.hpp
@@ -0,0 +1,20 @@
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap: ClapTrap {
+private:
+public:
+	ScavTrap() ;
+	ScavTrap( const std::string & name ) ;
+	ScavTrap(const ScavTrap& s) ;
+	ScavTrap& operator =(const ScavTrap& s) ;
+	~ScavTrap() ;
+	// member functions
+	void	attack(const std::string& target) ;
+	void	takeDamage(unsigned int amount) ;
+	void	beRepaired(unsigned int amount) ;
+	void	guardGate() ;
+} ;
+#endif
