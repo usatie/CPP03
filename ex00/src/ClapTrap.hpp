b207fd8cc5cfc9c438d8d3bef4e0712da5d0da0e
Shun Usami <usatie@gmail.com>
2023-03-29 18:20:42 +0900

    :recycle: Refactor ClapTrap and main.cpp

--- ex00/src/ClapTrap.hpp
+++ ex00/src/ClapTrap.hpp
@@ -1,24 +1,27 @@
 #ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
+#define CLAPTRAP_HPP
 
-# include <string>
+#include <string>
 
 class ClapTrap {
-private:
-	std::string _name ;
-	int	_hitPoints ;
-	int	_energyPoints ;
-	int	_attackDamage ;
-public:
-	ClapTrap() ;
-	ClapTrap( const std::string & name ) ;
-	ClapTrap(const ClapTrap& x) ;
-	ClapTrap& operator =(const ClapTrap& x) ;
-	~ClapTrap() ;
-	// member functions
-	void	attack(const std::string& target) ;
-	void	takeDamage(unsigned int amount) ;
-	void	beRepaired(unsigned int amount) ;
-} ;
+ private:
+  std::string _name;
+  int _hitPoints;
+  int _energyPoints;
+  int _attackDamage;
+  // member functions
+  bool canMove() const;
+
+ public:
+  ClapTrap();
+  explicit ClapTrap(const std::string& name);
+  ClapTrap(const ClapTrap& c);
+  ClapTrap& operator=(const ClapTrap& c);
+  ~ClapTrap();
+  // member functions
+  void attack(const std::string& target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+};
 
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,44 +1,71 @@
-#include "ClapTrap.hpp"
 #include <iostream>
 
-int	main(void) {
-	// Attack / no more energy
-	{
-		ClapTrap ct("John") ;
+#include "ClapTrap.hpp"
 
-		for ( int i = 1; i <= 11; i++ ) {
-			std::cout << i << ": " ;
-			ct.attack("foo") ;
-		}
-		std::cout << std::endl ;
-	}
-	std::cout << std::endl ;
-	// be repaired / no more energy
-	{
-		ClapTrap ct("Bob") ;
+#define RESET "\033[m"
+#define G "\e[0;32m"
+#define B "\033[1m"
+#define R "\e[0;31m"
+#define GREEN(s) (std::cout << G << s << RESET << std::endl)
+#define BOLD(s) (std::cout << B << s << RESET << std::endl)
+#define RED(s) (std::cout << R << s << RESET << std::endl)
 
-		for ( int i = 1; i <= 11; i++ ) {
-			std::cout << i << ": " ;
-			ct.beRepaired(i) ;
-		}
-		std::cout << std::endl ;
-	}
-	std::cout << std::endl ;
-	// no hitpoint
-	{
-		ClapTrap ct("Mike") ;
+int main(void) {
+  // Orthodox Canonical Form
+  {
+    GREEN("Orthodox Canonical Form");
+    BOLD("Default Initialization");
+    ClapTrap a;  // Default Initialization (Default constructor)
+    BOLD("Direct Initialization");
+    ClapTrap b("B");  // Direct Initialization (Constructor)
+    BOLD("Direct Initialization");
+    ClapTrap c(b);  // Direct Initialization (Copy constructor)
+    BOLD("Copy Initialization");
+    ClapTrap d = c;  // Copy Initialization (Copy constructor)
+    BOLD("Copy Assignment");
+    a = d;  // (copy assignment operator)
+  }
+  // Attack / no more energy
+  {
+    GREEN("Attack / no more energy");
+    ClapTrap ct("John");
 
-		ct.takeDamage(10) ;
-		ct.beRepaired(5) ;
-		ct.attack("foo") ;
-	}
-	std::cout << std::endl ;
-	// no hitpoint
-	{
-		ClapTrap ct("Rob") ;
+    for (int i = 1; i <= 11; i++) {
+      std::cout << i << ": ";
+      ct.attack("foo");
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  // be repaired / no more energy
+  {
+    GREEN("beRepaired / no more energy");
+    ClapTrap ct("Bob");
 
-		ct.takeDamage(15) ;
-		ct.beRepaired(5) ;
-		ct.attack("foo") ;
-	}
+    for (int i = 1; i <= 11; i++) {
+      std::cout << i << ": ";
+      ct.beRepaired(i);
+    }
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
+  // no hitpoint
+  {
+    GREEN("no hitpoint1");
+    ClapTrap ct("Mike");
+
+    ct.takeDamage(10);
+    ct.beRepaired(5);
+    ct.attack("foo");
+  }
+  std::cout << std::endl;
+  // no hitpoint
+  {
+    GREEN("no hitpoint2");
+    ClapTrap ct("Rob");
+
+    ct.takeDamage(15);
+    ct.beRepaired(5);
+    ct.attack("foo");
+  }
 }
